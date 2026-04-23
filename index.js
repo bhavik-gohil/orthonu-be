46481415fa56bd456ad7fdda5e882c742bdb878c
bhavik <bhavik@mail.com>
2026-04-24 02:11:12 +0530

    ok

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(require('path').join(__dirname, 'public/uploads')));
 app.use('/', routes);
 
