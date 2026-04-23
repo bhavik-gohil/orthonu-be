77f33a1708f7ea0dee1bba5f2f6eb2b418fc93de
bhavik <bhavik@mail.com>
2026-04-23 18:50:56 +0530

    changes

--- config/config.js
+++ config/config.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
+
+const dbConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: process.env.DB_SSL === 'false' ? {} : {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  },
+  development: dbConfig,
+  production: dbConfig,
 };
