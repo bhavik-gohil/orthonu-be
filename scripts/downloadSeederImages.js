c0ae7b2cd8127e235d5e7a68db9011a3bb8b284d
bhavik <bhavik@mail.com>
2026-04-24 17:59:08 +0530

    ok

--- scripts/downloadSeederImages.js
+++ scripts/downloadSeederImages.js
@@ -60,4 +60,8 @@ async function run() {
   }
 }
 
-run();
+module.exports = run;
+
+if (require.main === module) {
+  run();
+}
--- seeders/runSeeders.js
+++ seeders/runSeeders.js
@@ -1,9 +1,12 @@
 const seedBoardMembers = require('./seedBoardMembers');
 const seedPartners = require('./seedPartners');
 const seedTestimonials = require('./seedTestimonials');
+const downloadImages = require('../scripts/downloadSeederImages');
 
 async function runAllSeeders() {
   console.log('Starting seeder execution...');
+  console.log('Downloading images first...');
+  await downloadImages();
   await seedBoardMembers();
   await seedPartners();
   await seedTestimonials();
