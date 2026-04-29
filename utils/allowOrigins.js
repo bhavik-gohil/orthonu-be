40283c84ced58d245b692cb03c8caf1c86a66f05
bhavik <bhavik@mail.com>
2026-04-29 14:48:59 +0530

    OK

--- utils/allowOrigins.js
+++ utils/allowOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   process.env.FE_BASE_URL,
   process.env.FE_SHOP_BASE_URL,
   process.env.FE_ADMIN_BASE_URL,
+  process.env.FE_START_BASE_URL,
 ].filter(Boolean);
 
 module.exports = allowedOrigins;
